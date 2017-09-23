45898d3553915ea6e444823aa6cecd46c3d11f9e
Aswin Mohan <aswinmmohan@gmail.com>
2017-09-23 21:24:28 +0530

    Finish Navigation Structure

--- src/appNavigation.js
+++ src/appNavigation.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-
 import { StackNavigator } from 'react-navigation';
+
+import TestComponentScreen from './screens/testComponentScreen';
 
 // Use StackNavigator by Default
 const AppNavigation = StackNavigator({
-
+    TestComponentScreen: {screen: TestComponentScreen}
 }, {
     navigationOptions:{
         header: null,
---
+++ src/screens/testComponentScreen.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+class TestComponentScreen extends Component {
+    render() {
+        return (
+            <Text>Testing...</Text>
+        );
+    }
+}
+
+export default TestComponentScreen;
