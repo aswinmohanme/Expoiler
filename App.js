e8272e03de4853c9a5aace2b9ebeda19c6811e80
Aswin Mohan <aswinmmohan@gmail.com>
2017-09-23 21:44:09 +0530

    Glue up Mobx Store

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'mobx-react';
 
 import AppNavigation from './src/appNavigation';
+import RootStore from './src/stores/rootStore';
 
 export default class App extends React.Component {
   render() {
     return (
-      <AppNavigation />
+      <Provider rootStore={ new RootStore()}>
+        <AppNavigation />
+      </Provider>
     );
   }
 }
